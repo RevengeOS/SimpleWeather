7571ef028cb781da05468c969c55583aa61f861f
Lucchetto <lucchetto.tie@live.com>
2020-12-06 14:58:16 +0100

    move mapConditionIconToCode function to iconshelper
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/revengeos/simpleweather/WeatherUtils.kt
+++ app/src/main/java/com/revengeos/simpleweather/WeatherUtils.kt
@@ -26,6 +26,7 @@ import androidx.preference.PreferenceManager
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.revengeos.simpleweather.data.WeatherData
+import com.revengeos.weathericons.WeatherIconsHelper
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -135,80 +136,6 @@ class WeatherUtils(private val context: Context) {
         val id = data!!.weather[0].id
         val sunrise = data.sys.sunrise
         val sunset = data.sys.sunset
-        return mapConditionIconToCode(id, sunrise, sunset)
-    }
-
-    private fun mapConditionIconToCode(conditionId: Int, sunrise: Long, sunset: Long): Int {
-        val ut2 = System.currentTimeMillis() / 1000L
-        if (ut2 in (sunrise + 1) until sunset) {
-            // First, use condition ID for specific cases
-            when (conditionId) {
-                202, 232, 211 -> return 4
-                212 -> return 3
-                221, 231, 201 -> return 38
-                230, 200, 210 -> return 37
-                300, 301, 302, 310, 311, 312, 313, 314, 321 -> return 9
-                500, 501, 520, 521, 531 -> return 11
-                502, 503, 504, 522 -> return 12
-                511 -> return 10
-                600, 620 -> return 14 // light snow
-                601, 621 -> return 16 // snow
-                602, 622 -> return 41 // heavy snow
-                611, 612 -> return 18 // sleet
-                615, 616 -> return 5 // rain and snow
-                741 -> return 20
-                711, 762 -> return 22
-                701, 721 -> return 21
-                731, 751, 761 -> return 19
-                771 -> return 23
-                781 -> return 0
-                800 -> return 32
-                801 -> return 34
-                802 -> return 28
-                803, 804 -> return 30
-                900 -> return 0 // tornado
-                901 -> return 1 // tropical storm
-                902 -> return 2 // hurricane
-                903 -> return 25 // cold
-                904 -> return 36 // hot
-                905 -> return 24 // windy
-                906 -> return 17 // hail
-            }
-        } else {
-            // First, use condition ID for specific cases
-            when (conditionId) {
-                202, 232, 211 -> return 4
-                212 -> return 3
-                221, 231, 201 -> return 47
-                230, 200, 210 -> return 45
-                300, 301, 302, 310, 311, 312, 313, 314, 321 -> return 9
-                500, 501, 520, 521, 531 -> return 11
-                502, 503, 504, 522 -> return 12
-                511 -> return 10
-                600, 620 -> return 14 // light snow
-                601, 621 -> return 16 // snow
-                602, 622 -> return 41 // heavy snow
-                611, 612 -> return 18 // sleet
-                615, 616 -> return 5 // rain and snow
-                741 -> return 20
-                711, 762 -> return 22
-                701, 721 -> return 21
-                731, 751, 761 -> return 19
-                771 -> return 23
-                781 -> return 0
-                800 -> return 31
-                801 -> return 33
-                802 -> return 27
-                803, 804 -> return 29
-                900 -> return 0 // tornado
-                901 -> return 1 // tropical storm
-                902 -> return 2 // hurricane
-                903 -> return 25 // cold
-                904 -> return 36 // hot
-                905 -> return 24 // windy
-                906 -> return 17 // hail
-            }
-        }
-        return -1
+        return WeatherIconsHelper.mapConditionIconToCode(id, sunrise, sunset)
     }
 }
